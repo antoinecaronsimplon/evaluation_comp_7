febfb4e8bcfe500c4ed3aecfe2eaa04692b46197
antoinecaronsimplon <antoinecaron.simplon@gmail.com>
2018-09-10 10:25:08 +0200

    first commit

---
+++ prestashop_1.7.4.2/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'devweb',
+    'database_password' => 'root',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'vY9Sar8t7nid1G3aH0TjJA7gQQgPfucFsDcXiywqZNgseo9LsPtxmQdn',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-09-10',
+    'locale' => 'fr-FR',
+    'cookie_key' => 'PKwN0Mu1ImbK75JNhaS7S2ECvBM3AbfeqNLAngzVTuz8ogn9BZ7E0v6H',
+    'cookie_iv' => '8nayH3tv',
+    'new_cookie_key' => 'def0000044a99d710c13ba3f88bac925d85bb7339a122156328b7fc73a9c05a85b3716883dec3e861b5a0848b0c20f2bb55c4a2a81dca10edd7628e26e9b71b03fbecbd6',
+  ),
+);
